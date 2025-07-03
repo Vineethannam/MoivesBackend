e34fb9c4d76f7273e242965daa2eb38ac7e07699
Annam Vineeth <vineethannam5@gamil.com>
2025-07-03 22:51:40 +0530

    Initial Commit

---
+++ src/main/java/com/vineeth/Moives/MoivesApplication.java
@@ -0,0 +1,14 @@
+package com.vineeth.Moives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoivesApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoivesApplication.class, args);
+	}
+
+}
